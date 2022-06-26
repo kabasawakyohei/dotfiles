eb4d36ffead7cce69d8e2c31abce0eeee4a386ca
kyohei kabasawa <slifekb2@gmail.com>
2022-06-26 11:08:34 +0900

    create .alizses.zsh

---
+++ .aliases.zsh
@@ -0,0 +1,11 @@
+# ==============================
+# General
+# ==============================
+alias vim='nvim'
+alias reload='source ~/.zshenv && source ~/.zshrc'
+
+# ==============================
+# doceker
+# ==============================
+alias lzd='lazydocker'
+alias da='docker compose'
